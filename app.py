1b3da2994f7f66f9262841f9dd999549df0813ad
nicosiaivonne-create <nicosiaivonne@gmail.com>
2026-05-27 23:02:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -515,9 +515,7 @@ elif st.session_state.rol_sel == "SUPERVISOR":
         with c4: st.number_input("Combustible (Lts):", value=0.0, key="sup_combustible")
         st.markdown('</div>', unsafe_allow_html=True)
          # --- BOTÓN DE PÁNICO INTEGRADO ---
-        if st.button("🚨 ACTIVAR PÁNICO", use_container_width=True):
-            accionar_panico_sup()
-        # ---------------------------------
+        
         col_btn1, col_btn2 = st.columns([3, 1])
         with col_btn1: st.button("SELLAR ODOMETRÍA Y LOGÍSTICA", key="btn_sellar_logistica", use_container_width=True)
         with col_btn2:
@@ -529,7 +527,11 @@ elif st.session_state.rol_sel == "SUPERVISOR":
             opciones_servicios = df_objetivos_filtrados['OBJETIVO'].unique() if not df_objetivos_filtrados.empty else ["SIN OBJETIVOS"]
             obj_seleccionado_sup = st.selectbox("SERVICIO ACTUAL:", opciones_servicios, key="sup_servicio_actual")
             st.radio("ACCIÓN:", ["SELECCIONAR...", "INGRESO", "SALIDA"], index=0, key="sup_radio_accion", horizontal=True)
-           
+           # --- AQUÍ VA EL BOTÓN DE PÁNICO ---
+            st.write("---") # Una línea divisoria para separar visualmente
+            if st.button("🚨 ACTIVAR PÁNICO", use_container_width=True):
+                accionar_panico_sup()
+            # ---------------------------------
             st.write("---")
             df_mapa_sup = df_objetivos_filtrados.dropna(subset=['LATITUD', 'LONGITUD'])
             if not df_mapa_sup.empty:
