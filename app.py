66c78c8d8fcd03b17b1dacce34995dc630cd9ec3
nicosiaivonne-create <nicosiaivonne@gmail.com>
2026-06-04 11:12:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -478,6 +478,7 @@ if 'obj_seleccionado' in locals() and obj_seleccionado != "MOSTRAR TODO":
 
 # Renderizado final del mapa DENTRO de la pestaña
 st_folium(m_mon, width="100%", height=550, key="mapa_monitoreo_radar_tactico")
+
     with t_gestion:
         st.subheader("📖 HISTORIAL DE OPERATIVOS")
         if not df_emergencias.empty:
