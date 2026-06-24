fa196bae04c6eaa7c93eeda829da061c9c9ad269
nicosiaivonne-create <nicosiaivonne@gmail.com>
2026-06-23 23:06:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -357,8 +357,12 @@ if st.session_state.rol_sel == "MONITOREO":
         "🚨 RADAR S.O.S", label_mensajeria, "👥 PADRÓN VIGILADORES", "🔄 NOVEDADES Y FICHAJES"
     ])
 
-st.markdown('<div class="panel-novedad">', unsafe_allow_html=True)
-    col_sel1, col_sel2 = st.columns([2, 1])
+# --- INTERFAZ DE SELECCIÓN Y ANÁLISIS TÁCTICO ---
+        st.markdown('<div class="panel-novedad">', unsafe_allow_html=True)
+        col_sel1, col_sel2 = st.columns([2, 1])
+
+        if "filtro_radar_valor" not in st.session_state:
+            st.session_state["filtro_radar_valor"] = "MOSTRAR TODO"
         
         if "filtro_radar_valor" not in st.session_state:
             st.session_state["filtro_radar_valor"] = "MOSTRAR TODO"  
