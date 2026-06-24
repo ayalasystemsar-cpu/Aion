045cc7814a32ffc4e9e8894c63338838eece7981
nicosiaivonne-create <nicosiaivonne@gmail.com>
2026-06-23 22:58:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -356,13 +356,12 @@ if st.session_state.rol_sel == "MONITOREO":
     t_radar, t_comunicacion, t_vig, t_nov = st.tabs([
         "🚨 RADAR S.O.S", label_mensajeria, "👥 PADRÓN VIGILADORES", "🔄 NOVEDADES Y FICHAJES"
     ])
-            # --- INTERFAZ DE SELECCIÓN Y ANÁLISIS TÁCTICO ---
+        # --- INTERFAZ DE SELECCIÓN Y ANÁLISIS TÁCTICO ---
         st.markdown('<div class="panel-novedad">', unsafe_allow_html=True)
         col_sel1, col_sel2 = st.columns([2, 1])
         
         if "filtro_radar_valor" not in st.session_state:
-            st.session_state["filtro_radar_valor"] = "MOSTRAR TODO"
-        
+            st.session_state["filtro_radar_valor"] = "MOSTRAR TODO"  
 
         with col_sel1:
             opciones_busqueda = ["MOSTRAR TODO"] + list(df_mapa_monitoreo['OBJETIVO'].unique()) if not df_mapa_monitoreo.empty else ["MOSTRAR TODO"]
