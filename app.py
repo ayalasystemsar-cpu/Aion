5416d2e394179cf7f642e6fce79802df66648a3c
nicosiaivonne-create <nicosiaivonne@gmail.com>
2026-06-24 17:25:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1028,6 +1028,8 @@ elif st.session_state.rol_sel == "JEFE DE OPERACIONES":
 elif st.session_state.rol_sel == "GERENCIA":
     st.markdown('<h2 style="color:#00E5FF; font-family:\'Orbitron\', sans-serif; font-size:24px; margin-bottom:5px;">Comando Estratégico: DIRECCIÓN GENERAL</h2>', unsafe_allow_html=True)
     elif st.session_state.rol_sel == "GERENCIA":
+
+       elif st.session_state.rol_sel == "GERENCIA":
     # 1. Calculamos el total de mensajes pendientes para GERENCIA
     df_msg = leer_matriz_nube("MENSAJERIA")
     nombre_user = st.session_state.user_sel.upper()
@@ -1050,8 +1052,7 @@ elif st.session_state.rol_sel == "GERENCIA":
     
     # 4. Pestaña de Mensajería Global
     with t_mensajeria_ger:
-        renderizar_mensajeria_global("GERENCIA")
-    
+        renderizar_mensajeria_global("GERENCIA") 
 
     # 3. Pestaña de EJECUCIÓN (Restaurada)
     with t_ejecucion_ger:
