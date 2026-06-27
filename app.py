60ef212d9e6f5b1792f91a35b20af14096795205
nicosiaivonne-create <nicosiaivonne@gmail.com>
2026-06-27 03:04:31 -0300

    Add logout button functionality in app.py

--- app.py
+++ app.py
@@ -510,6 +510,13 @@ with st.sidebar:
         st.session_state.user_sel = "ADMIN CENTRAL"
         st.session_state.sup_autenticado = False
         st.rerun()
+        
+        st.markdown("---")
+    if st.button("🚪 CERRAR SESIÓN", use_container_width=True):
+        st.session_state.usuario_logueado = False
+        st.rerun()
+
+    
 
     st.write("---")
     
